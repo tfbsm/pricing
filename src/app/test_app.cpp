cf0f72b875724378262907f580884aab05d796b0
Andrey Gumirov <andreyt45@gmail.com>
2025-12-15 22:00:17 +0700

    build: add build with zeromq

--- src/app/test_app.cpp
+++ src/app/test_app.cpp
@@ -10,23 +10,23 @@ typedef std::chrono::steady_clock clock_;
 
 int main(int argc, char const* argv[]) {
     // Create ZeroMQ context
-    zmq::context_t context(1);
-    // tfbsm::StableDistribution sd{0.5, 1.0};
+    // zmq::context_t context(1);
+    tfbsm::StableDistribution sd{0.5, 1.0};
 
     // tfbsm::PricingEngine pe{.5, 1e-2};
 
-    // std::vector<double> values(1e5);
+    std::vector<double> values(1e5);
 
-    // clock_::time_point begin = clock_::now();
+    clock_::time_point begin = clock_::now();
 
-    // for (int i = 0; i < 1e3; ++i) {
-    //     sd.sampleValues(values);
-    // }
+    for (int i = 0; i < 1e3; ++i) {
+        sd.sampleValues(values);
+    }
 
-    // clock_::time_point end = clock_::now();
+    clock_::time_point end = clock_::now();
 
-    // std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 1e3
-    //           << std::endl;
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 1e3
+              << std::endl;
 
     return 0;
 }
