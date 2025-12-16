8565f09fb53f09cb8ae3d1e216c3857ed0eff6d8
Andrey Gumirov <andreyt45@gmail.com>
2025-12-16 23:45:05 +0700

    feat: add configrepo and option json serialization

--- include/libtfbsm/option.hpp
+++ include/libtfbsm/option.hpp
@@ -3,6 +3,8 @@
 #include "stdint.h"
 #include "string"
 
+#include "nlohmann/json.hpp"
+
 namespace tfbsm {
 
 class Option {
@@ -15,8 +17,35 @@ public:
 
     time_t expiration_time_stamp;
 
+    Option() = default;
+
     Option(OptionType type, double strike_price, time_t expiration_time_stamp)
             : type(type), strike_price(strike_price), expiration_time_stamp(expiration_time_stamp){};
+
+    
+    void from_json(const nlohmann::json& j) {
+        j.at("symbol").get_to(symbol);
+        j.at("strike_price").get_to(strike_price);
+        j.at("expiration").get_to(expiration_time_stamp);
+
+        auto _type = j.at("type").get<std::string>();
+        if (_type == "call") {
+            type = OptionType::kCall;
+        } else if (_type == "put") {
+            type = OptionType::kPut;
+        }
+    }
+
+    void to_json(nlohmann::json& j) const noexcept {
+        auto type_str = type == OptionType::kCall ? "call" : "put";
+
+        j = nlohmann::json{
+            {"symbol", symbol},
+            {"type", type_str},
+            {"strike_price", strike_price},
+            {"expiration", expiration_time_stamp}
+        };
+    }
 };
 
 }  // namespace tfbsm
